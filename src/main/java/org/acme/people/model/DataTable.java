af10f06f50b5062cac2334704a880eb1c91f22e7
Alex Groom <alex.groom@gmail.com>
2023-02-06 09:16:51 +0000

    fixup otel

--- src/main/java/org/acme/people/model/DataTable.java
+++ src/main/java/org/acme/people/model/DataTable.java
@@ -3,11 +3,10 @@ package org.acme.people.model;
 import java.io.Serializable;
 import java.util.List;
 import org.acme.people.utils.Sleep;
-import org.eclipse.microprofile.opentracing.Traced;
 import javax.enterprise.context.ApplicationScoped;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @ApplicationScoped
-@Traced
 public class DataTable implements Serializable {
 
     private static final long serialVersionUID = -7304814269819778382L;
@@ -17,39 +16,50 @@ public class DataTable implements Serializable {
     public List<Person> data;
     public String error;
 
+    @WithSpan
     public DataTable() {
 
     }
 
+    @WithSpan
     public void setDraw(int draw) {
         this.draw = draw;
     }
+    @WithSpan
     public void setRecordsTotal(long recordsTotal) {
         this.recordsTotal = recordsTotal;
     }
+    @WithSpan
     public void setRecordsFiltered(long recordsFiltered) {
         Sleep.pause(200);
         this.recordsFiltered = recordsFiltered;
     }
+    @WithSpan
     public void setData(List<Person> data) {
         this.data = data;
     }
+    @WithSpan
     public void setError(String error) {
         this.error = error;
     }
 
+    @WithSpan
     public long getDraw() {
         return this.draw;
     }
+    @WithSpan
     public long getRecordsTotal() {
         return this.recordsTotal;
     }
+    @WithSpan
     public long getRecordsFiltered() {
         return this.recordsFiltered;
     }
+    @WithSpan
     public List<Person> getData() {
         return this.data;
     }
+    @WithSpan
     public String getError() {
         return this.error;
     }
